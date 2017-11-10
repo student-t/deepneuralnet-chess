43aeeb84dd2024850cbfec5ca5a65c4f1fccdb27
Marshal Hayes <mdhayes.07@gmail.com>
2017-11-10 15:39:25 -0600

    requirements.txt

--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from distutils.core import setup
-
-setup(name='deepneuralnet-chess',
-    version='0.1',
-    description='deepneural-net-chess package',
-    author='Marshal Hayes',
-    auth_email='mhayes8@memphis.edu')
